62b4a55bc56b56164cdfc89c0665d10353d39ac6
Luca Capriati <lucapriati246@gmail.com>
2024-07-04 18:59:39 +0200

    Added Light/Dark Mode Toggle
    - Implemented a toggle button for the theme of the website using Shadcn and their Nextj docs.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import {Toaster} from '@/components/ui/toaster';
+import {ThemeProvider} from "@/components/providers/ThemeProvider";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -20,16 +21,18 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
-        <Navbar/>
-        <div className={'flex'}>
-            <div className={'hidden md:block h-dvh w-[350px]'}>
-                <Sidebar/>
+        <ThemeProvider attribute={'class'} defaultTheme={'light'} enableSystem={true} storageKey={'dashboard-theme'}>
+            <Navbar/>
+            <div className={'flex'}>
+                <div className={'hidden md:block h-dvh w-[350px]'}>
+                    <Sidebar/>
+                </div>
+                <div className={'px-5 w-full md:max-w-[1140px]'}>
+                    {children}
+                </div>
             </div>
-            <div className={'px-5 w-full md:max-w-[1140px]'}>
-                {children}
-            </div>
-        </div>
-        <Toaster />
+            <Toaster/>
+        </ThemeProvider>
         </body>
         </html>
     );
